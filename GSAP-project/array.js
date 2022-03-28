e88afcc4e5a72d9f6f99e001a08b695a838f5879
Henryla51 <henryhen2019@gmail.com>
2022-03-28 10:13:19 -0400

    1

--- GSAP-project/array.js
+++ GSAP-project/array.js
@@ -13,4 +13,93 @@ let Attractions = [
     subtitle: "2.Sydney Opera House",
     text: "One of the world's great icons, the Sydney Opera House is a UNESCO World Heritage Site and the star attraction on the glittering harbor. This graceful building, shaped like shells or billowing sails, perches on a finger of land surrounded by water.",
   },
+  {
+    name: "Queen Victoria Building",
+    image:
+      "https://images.unsplash.com/photo-1565016106133-d5f7db11fdb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cXVlZW4lMjB2aWN0b3JpYSUyMGJ1aWxkaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    subtitle: "3. Queen Victoria Building",
+    text: "Opened in 1898, initially as the Sydney Produce Market, Queen Victoria Building, or QVB, as locals know it, is a shopping and dining destination located three blocks west of Hyde Park, taking up an entire block in Sydney’s CBD, or Central Business District. One of the most famous landmarks in Sydney, QVB is home to around 190 retail outlets. Browse iconic Australian brands, including Lorna Jane for activewear, R.M. Williams for men’s, women’s, and children’s apparel, and Hunter’s for gorgeous cashmere. Pick up some confectionery from Australia’s oldest family-owned chocolate makers, Haigh’s Chocolates, and invest in artwork at Aboriginal Art Galleries, which offers fully-insured, free worldwide shipping.",
+  },
+  {
+    name: "Sydney Tower Eye",
+    image:
+      "https://images.unsplash.com/photo-1539424865926-b103c06a6d6e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8c3lkbmV5JTIwdG93ZXIlMjBleWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    subtitle: "4. Sydney Tower Eye",
+    text: "An icon of the city’s skyline, the pin-like Sydney Tower Eye is one of the most famous Sydney landmarks and the city’s tallest building at 1,014 ft high. Take in a remarkable bird’s eye view from 880 feet up on a 60-minute Skywalk tour, one floor above the indoor Observation Deck. Views are said to stretch as far as the Blue Mountains range, 50 miles northwest of the city. Take binoculars to spot other Sydney landmarks, including Botany Bay to the south and Sydney Harbour to the north. Book a ticket that includes a dining experience at SkyFeast, Sydney Tower Eye’s 82nd story revolving restaurant. Designer stores, the Westfield Shopping Mall, and plenty of restaurants and bars can be found dotted around Sydney Tower Eye’s Market Street location.",
+  },
+];
+let food = [
+  {
+    name: "Barramundi ",
+    image:
+      "https://images.unsplash.com/photo-1641864624972-fb82b7489666?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmFycmFtdW5kaSUyMGRpc2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    subtitle: "1. Barramundi",
+    text: "Barramundi is a quintessential Australian fish that’s served in restaurants all over the country but is particularly common in Sydney. Native to Australia and the Indo-Pacific, barramundi is a hardy species. They’re naturally high in heart-healthy Omega-3 fatty acids and can be easily farmed without the use of antibiotics or hormones. They’re also a great source of lean protein with a gentle, mild flavor and toothsome texture, and it’s virtually impossible to overcook. Like common types of whitefish, barramundi lacks a strong odour and can be used in a variety of recipes.",
+  },
+  {
+    name: "Avacado On Toast",
+    image:
+      "https://images.unsplash.com/photo-1541519227354-08fa5d50c44d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhY2FkbyUyMG9uJTIwdG9hc3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    subtitle: "2. Avacado On Toast",
+    text: "Avocado is a nutritious staple in many Australian recipes, especially for breakfast. Avocado is often mashed onto 1, 2 or 3 slices of toasted, crusty bread for a morning dose of fat and flavor that’s different from butter or margarine. In some spreads, avocado may be combined with feta cheese, sprinkled with sesame seeds, drizzled with olive oil or topped with poached eggs for a unique blend of flavors. You may also find avocado on toast served with black sesame and beetroot hummus, adding some strong flavors and additional nutritional benefits to your breakfast.",
+  },
+  {
+    name: "Bacon and Egg Roll",
+    image:
+      "https://www.sbs.com.au/food/sites/sbs.com.au.food/files/styles/full/public/bacon-egg-roll-hp-sauce_0.jpg?itok=j1IZaMdX",
+    subtitle: "3. Bacon and Egg Roll",
+    text: "Bacon and eggs for breakfast is a British tradition that influenced Australian mornings, but the dish has been transformed into a portable breakfast snack that’s distinctive to the city or region. In Sydney, the bacon and egg roll may be combined with caramelized onions and fresh tomato relish served on an artisan bread roll and filled with grains or seeds. They can also be stuffed with smoked bacon and a fried egg on fresh-baked bread. Whichever way it’s prepared, the bacon and egg roll is a quintessential breakfast food that pairs nutrients with local flavors for a hearty, satisfying morning meal.",
+  },
+  {
+    name: "Lammington Cake",
+    image:
+      "https://img.taste.com.au/TDWXxhbY/taste/2016/11/lamington-cake-63168-1.jpeg",
+    subtitle: "4. Lammington Cake",
+    text: "Another local delicacy which is sometimes pondered as a national cake of Australia.Its a very moist, sweet square sponge cake that’s soaked in rich chocolate sauce and rolled in flaky coconut. I mean imagine the wonders of chocolatey creaminess and zesty tint of coconut flakes is superb to be infused in a popular food to eat in Sydney. Lamingtons can be found in numerous versions of flavors at various bakeries using creative twists of the bakers on this classic recipe. If you adore jam in your cakes then they can also add jam and cream to the center of cake that makes the dessert richer in texture and topping of salted caramel or dulce de leche is just another level dessert which you might not find anywhere in the world.",
+  },
+  {
+    name: "Yellow Fin Tuna",
+    image: "",
+    subtitle: "5. Yellow Fin Tuna",
+    text: "Yellowfin tuna is a common fish used in sashimi and sushi, but its abundant population in Australian waters makes this one of the most popular fish in Sydney and among the best food in Australia. Similar to steak, yellowfin tuna has a mild, meaty flavor, with bright-red meat that is firm, moist and flaky, making it a great choice to consume raw. Yellowfin tuna cutlets and steaks can be cooked by broiling, baking, smoking, poaching, marinating or barbecuing and are usually best when seared on the outside and left red on the inside. When paired with powerful flavors, such as charred capsicum, balsamic vinegar, eggplant, wasabi or bitter greens, the taste of yellowfin becomes even more distinctive.",
+  },
+  {
+    name: "N2 Extreme Gelato",
+    image:
+      "https://images.gessato.com/NvJJUR8.OWDa~b707/w:auto/h:auto/q:100/https://www.gessato.com/wp-content/uploads/2018/07/n2-extreme-gelato.jpg",
+    subtitle: "6. N2 Extreme Gelato",
+    text: "A delicious dessert to end your food tour in Sydney on a good sugary note is the N2 extreme gelato which gives you a good reason to enjoy the last bites as a cold, creamy, jelly-like dessert. Along with its interesting concept of making and serving ice cream, you won’t forget this dessert. There are long ques of locals and tourists flocking near the gelato trucks and bars to have a taste of some of the refined super tasty desserts. ",
+  },
+];
+
+let Souvenirs = [
+  {
+    name: "Dinosaur Designs",
+    image:
+      "https://www.gpsmycity.com/img/art_item/0f75e4342e9-dinosaur-designs.jpg",
+    subtitle: "1. Dinosaur Designs",
+    text: "Sydney-siders, Louise Olsen, Stephen Ormandy and Liane Rossler formed Dinosaur Designs in 1985, with the first of their now-famous jewelry sold at Sydney’s Paddington markets. The design trio have gone on to achieve phenomenal success in the design world, with their hand-cast resin jewelry gracing the pages of high fashion magazines and accessorizing the outfits of numerous celebrities. Bangles and rings are the bedrock of Dinosaur Design’s collections, with bangles starting from AU$55.00 and rings costing upwards of AU$50.00. They have also branched into homewares and metal work.",
+    link: "https://www.dinosaurdesigns.com.au/",
+  },
+  {
+    name: "Akubra Hats",
+    image: "",
+    subtitle: "2. Akubra Hats",
+    text: "Made famous by the 1980s film franchise ‘Crocodile Dundee’ the Akubra hat is an Australian staple. Akubras are crafted from rabbit fur felt. Paired with a “driza-bone” oiled leather coat, Akubras are one half of the classic Australian stockman’s outfit, which was the official outfit of the 2007 APEC Leader’s conference held in Sydney. Akubra’s are wide brimmed, designed to ward off the burning Australian sun and typically have a thin leather band around the base of the crown. This is sometimes substituted for a bullet belt by the real outback “Aussies.” Akubra hats range in price from AU$110 to AU$164, depending on the style.",
+    link: "https://akubra.com.au/",
+  },
+  {
+    name: "Kangaroo Leather",
+    image: "",
+    subtitle: "3. Kangaroo Leather",
+    text: "Sydney boasts some of the highest-quality kangaroo leather, carefully crafted into wallets, bags, belts and other goods. Kangaroo leather is prized because of its unique cross fibring, which allows it to be split very thinly without losing strength. It is considered the strongest type of leather for its weight. Prices vary according to the type of leather goods you choose, but a kangaroo leather wallet will set you back upwards of AU$29.95. A hand-embossed kangaroo leather wallet by a local aboriginal artist is AU$65.00.",
+    link: "https://www.therealmccaul.com/pages/kangaroo-leather",
+  },
+  {
+    name: "Gold Plated Eucalyptus Leaves",
+    image: "",
+    subtitle: "4. Gold Plated Eucalytus Leaves",
+    text: "The eucalyptus tree is an Australian native, and Sydney is surrounded by ancient eucalypt forests. The skeletons of these eucalypt leaves are collected by Bush Art Australia and gold plated to form the most delicate and beautiful jewelry. Necklaces with a single leaf cost upward of AU$25.00, and a pair of earrings with one leaf on each earring - upward of $AU40.00. Brooches and necklace, and earring sets are also available, as well as gold-plated gumnuts.",
+    link: "https://www.realleafjewelleryaustralia.com.au/products/copy-of-gold-eucalyptus-leaf-pendant",
+  },
 ];
