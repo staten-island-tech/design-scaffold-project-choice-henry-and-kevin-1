19e45e6b0f6b279b6894b43aa313fdf4cab99c95
Kevin <quickmathisgood@gmail.com>
2022-04-04 10:10:00 -0400

    ...

--- GSAP-project/array.js
+++ GSAP-project/array.js
@@ -13,4 +13,49 @@ let Attractions = [
     subtitle: "2.Sydney Opera House",
     text: "One of the world's great icons, the Sydney Opera House is a UNESCO World Heritage Site and the star attraction on the glittering harbor. This graceful building, shaped like shells or billowing sails, perches on a finger of land surrounded by water.",
   },
+  {
+    name: "Queen Victoria Building",
+    image: "",
+    subtitle: "3.Queen Victoria Building",
+    text: "Opened in 1898, initially as the Sydney Produce Market, Queen Victoria Building, or QVB, as locals know it, is a shopping and dining destination located three blocks west of Hyde Park, taking up an entire block in Sydney’s CBD, or Central Business District. One of the most famous landmarks in Sydney, QVB is home to around 190 retail outlets.Browse iconic Australian brands, including Lorna Jane for activewear, R.M.Williams for men’s, women’s, and children’s apparel, and Hunter’s for gorgeous cashmere. Pick up some confectionery from Australia’s oldest family- owned chocolate makers, Haigh’s Chocolates, and invest in artwork at Aboriginal Art Galleries, which offers fully - insured, free worldwide shipping."
+  },
+  {
+    name: "Sydney Tower Eye",
+    image: "",
+    subtitle: "4.Sydney Tower Eye",
+    text: "An icon of the city’s skyline, the pin-like Sydney Tower Eye is one of the most famous Sydney landmarks and the city’s tallest building at 1,014 ft high.Take in a remarkable bird’s eye view from 880 feet up on a 60-minute Skywalk tour, one floor above the indoor Observation Deck. Views are said to stretch as far as the Blue Mountains range, 50 miles northwest of the city. Take binoculars to spot other Sydney landmarks, including Botany Bay to the south and Sydney Harbour to the north. Book a ticket that includes a dining experience at SkyFeast, Sydney Tower Eye’s 82nd story revolving restaurant. Designer stores, the Westfield Shopping Mall, and plenty of restaurants and bars can be found dotted around Sydney Tower Eye’s Market Street location."
+  },
+];
+
+let Food = [
+  {
+    name: "Barramundi",
+    image: "",
+    text: "Barramundi is a quintessential Australian fish that’s served in restaurants all over the country but is particularly common in Sydney. Native to Australia and the Indo-Pacific, barramundi is a hardy species. They’re naturally high in heart-healthy Omega-3 fatty acids and can be easily farmed without the use of antibiotics or hormones. They’re also a great source of lean protein with a gentle, mild flavor and toothsome texture, and it’s virtually impossible to overcook. Like common types of whitefish, barramundi lacks a strong odour and can be used in a variety of recipes.",
+  },
+  {
+    name: "Avocado On Toast",
+    image: "",
+    text: "Avocado is a nutritious staple in many Australian recipes, especially for breakfast. Avocado is often mashed onto 1, 2 or 3 slices of toasted, crusty bread for a morning dose of fat and flavor that’s different from butter or margarine. In some spreads, avocado may be combined with feta cheese, sprinkled with sesame seeds, drizzled with olive oil or topped with poached eggs for a unique blend of flavors. You may also find avocado on toast served with black sesame and beetroot hummus, adding some strong flavors and additional nutritional benefits to your breakfast.",
+  },
+  {
+    name: "Bacon And Egg Roll",
+    image: "",
+    text: "Bacon and eggs for breakfast is a British tradition that influenced Australian mornings, but the dish has been transformed into a portable breakfast snack that’s distinctive to the city or region. In Sydney, the bacon and egg roll may be combined with caramelized onions and fresh tomato relish served on an artisan bread roll and filled with grains or seeds. They can also be stuffed with smoked bacon and a fried egg on fresh-baked bread. Whichever way it’s prepared, the bacon and egg roll is a quintessential breakfast food that pairs nutrients with local flavors for a hearty, satisfying morning meal.",
+  },
+  {
+    name: "Lamington Cake",
+    image: "",
+    text: "",
+  },
+  {
+    name: "Yellowfin Tuna",
+    image: "",
+    text: "",
+  },
+  {
+    name: " N2 Extreme Gelato",
+    image: "",
+    text: "",
+  },
 ];
