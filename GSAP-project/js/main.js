990380d6141373fa28140716acb2d7cb0b3389b5
Henryla51 <henryhen2019@gmail.com>
2022-04-05 10:10:54 -0400

    Update main.js

--- GSAP-project/js/main.js
+++ GSAP-project/js/main.js
@@ -1,1 +1,22 @@
-import "./array.js";
+import "../js/array";
+
+const DOMSelectors = {
+  cards: document.querySelector(".cards"),
+  search: document.getElementById("search"),
+};
+
+function defaultCardGenerate(filterAtractions) {
+  filterAttractions.forEach((Attractions) => {
+    DOMSelectors.cards.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card" id="${Attractions.name}">
+          <h2 class="characterName">${Attractions.name}</h2>
+          <img class="img" src="${Attractions.image}"/>
+          <div class="text">
+          <h3>${Attractions.subtitle}</h3>
+          <h3>${Attractions.text}</h3>
+          </div>
+      </div>`
+    );
+  });
+}
