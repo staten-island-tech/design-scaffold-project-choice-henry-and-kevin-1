020a7c281d2ede78d7222851d95a7e1624e1e060
Henryla51 <90654629+Henryla51@users.noreply.github.com>
2022-04-13 07:21:40 -0400

    check in

--- GSAP-project/js/array.js
+++ GSAP-project/js/array.js
@@ -1,4 +1,4 @@
-let Attractions = [
+export const attractions = [
   {
     name: "Sydney Harbor Bridge",
     image:
@@ -32,13 +32,14 @@ let Attractions = [
     id: 4,
   },
 ];
-let foods = [
+export const foods = [
   {
     name: "Barramundi ",
     image:
       "https://images.unsplash.com/photo-1641864624972-fb82b7489666?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmFycmFtdW5kaSUyMGRpc2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
     subtitle: "1. Barramundi",
     text: "Barramundi is a quintessential Australian fish that’s served in restaurants all over the country but is particularly common in Sydney. Native to Australia and the Indo-Pacific, barramundi is a hardy species. They’re naturally high in heart-healthy Omega-3 fatty acids and can be easily farmed without the use of antibiotics or hormones. They’re also a great source of lean protein with a gentle, mild flavor and toothsome texture, and it’s virtually impossible to overcook. Like common types of whitefish, barramundi lacks a strong odour and can be used in a variety of recipes.",
+    id: 1,
   },
   {
     name: "Avacado On Toast",
@@ -46,6 +47,7 @@ let foods = [
       "https://images.unsplash.com/photo-1541519227354-08fa5d50c44d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhY2FkbyUyMG9uJTIwdG9hc3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
     subtitle: "2. Avacado On Toast",
     text: "Avocado is a nutritious staple in many Australian recipes, especially for breakfast. Avocado is often mashed onto 1, 2 or 3 slices of toasted, crusty bread for a morning dose of fat and flavor that’s different from butter or margarine. In some spreads, avocado may be combined with feta cheese, sprinkled with sesame seeds, drizzled with olive oil or topped with poached eggs for a unique blend of flavors. You may also find avocado on toast served with black sesame and beetroot hummus, adding some strong flavors and additional nutritional benefits to your breakfast.",
+    id: 2,
   },
   {
     name: "Bacon and Egg Roll",
@@ -53,30 +55,35 @@ let foods = [
       "https://www.sbs.com.au/food/sites/sbs.com.au.food/files/styles/full/public/bacon-egg-roll-hp-sauce_0.jpg?itok=j1IZaMdX",
     subtitle: "3. Bacon and Egg Roll",
     text: "Bacon and eggs for breakfast is a British tradition that influenced Australian mornings, but the dish has been transformed into a portable breakfast snack that’s distinctive to the city or region. In Sydney, the bacon and egg roll may be combined with caramelized onions and fresh tomato relish served on an artisan bread roll and filled with grains or seeds. They can also be stuffed with smoked bacon and a fried egg on fresh-baked bread. Whichever way it’s prepared, the bacon and egg roll is a quintessential breakfast food that pairs nutrients with local flavors for a hearty, satisfying morning meal.",
+    id: 3,
   },
   {
     name: "Lammington Cake",
     image:
-      "https://img.taste.com.au/TDWXxhbY/taste/2016/11/lamington-cake-63168-1.jpeg",
+      "https://media.istockphoto.com/photos/lamingtons-picture-id182377523?b=1&k=20&m=182377523&s=170667a&w=0&h=dYD1UYUN3WlXtM2_yJeGNDADv6dWLEmd6TTfKiQPcGo=",
     subtitle: "4. Lammington Cake",
     text: "Another local delicacy which is sometimes pondered as a national cake of Australia.Its a very moist, sweet square sponge cake that’s soaked in rich chocolate sauce and rolled in flaky coconut. I mean imagine the wonders of chocolatey creaminess and zesty tint of coconut flakes is superb to be infused in a popular food to eat in Sydney. Lamingtons can be found in numerous versions of flavors at various bakeries using creative twists of the bakers on this classic recipe. If you adore jam in your cakes then they can also add jam and cream to the center of cake that makes the dessert richer in texture and topping of salted caramel or dulce de leche is just another level dessert which you might not find anywhere in the world.",
+    id: 4,
   },
   {
     name: "Yellow Fin Tuna",
-    image: "",
+    image:
+      "https://media.istockphoto.com/photos/yellowfin-tuna-catch-picture-id187275440?b=1&k=20&m=187275440&s=170667a&w=0&h=8fP2fn_3vkoRaTiIoCT2G1T-JP8pNjL3eqBZTjFWrkA=",
     subtitle: "5. Yellow Fin Tuna",
     text: "Yellowfin tuna is a common fish used in sashimi and sushi, but its abundant population in Australian waters makes this one of the most popular fish in Sydney and among the best food in Australia. Similar to steak, yellowfin tuna has a mild, meaty flavor, with bright-red meat that is firm, moist and flaky, making it a great choice to consume raw. Yellowfin tuna cutlets and steaks can be cooked by broiling, baking, smoking, poaching, marinating or barbecuing and are usually best when seared on the outside and left red on the inside. When paired with powerful flavors, such as charred capsicum, balsamic vinegar, eggplant, wasabi or bitter greens, the taste of yellowfin becomes even more distinctive.",
+    id: 5,
   },
   {
     name: "N2 Extreme Gelato",
     image:
-      "https://images.gessato.com/NvJJUR8.OWDa~b707/w:auto/h:auto/q:100/https://www.gessato.com/wp-content/uploads/2018/07/n2-extreme-gelato.jpg",
+      "https://media.istockphoto.com/photos/italian-gelato-of-various-flavors-in-ice-cream-parlor-picture-id1151706409?b=1&k=20&m=1151706409&s=170667a&w=0&h=zkM7vI-y70nslRrQKheOzm0WHDlUjYJ8usJxKWX3Tg8=",
     subtitle: "6. N2 Extreme Gelato",
     text: "A delicious dessert to end your food tour in Sydney on a good sugary note is the N2 extreme gelato which gives you a good reason to enjoy the last bites as a cold, creamy, jelly-like dessert. Along with its interesting concept of making and serving ice cream, you won’t forget this dessert. There are long ques of locals and tourists flocking near the gelato trucks and bars to have a taste of some of the refined super tasty desserts. ",
+    id: 6,
   },
 ];
 
-let Souvenirs = [
+export const souvenirs = [
   {
     name: "Dinosaur Designs",
     image:
@@ -84,26 +91,33 @@ let Souvenirs = [
     subtitle: "1. Dinosaur Designs",
     text: "Sydney-siders, Louise Olsen, Stephen Ormandy and Liane Rossler formed Dinosaur Designs in 1985, with the first of their now-famous jewelry sold at Sydney’s Paddington markets. The design trio have gone on to achieve phenomenal success in the design world, with their hand-cast resin jewelry gracing the pages of high fashion magazines and accessorizing the outfits of numerous celebrities. Bangles and rings are the bedrock of Dinosaur Design’s collections, with bangles starting from AU$55.00 and rings costing upwards of AU$50.00. They have also branched into homewares and metal work.",
     link: "https://www.dinosaurdesigns.com.au/",
+    id: 1,
   },
   {
     name: "Akubra Hats",
-    image: "",
+    image:
+      "https://media.istockphoto.com/photos/battered-old-cowboy-hat-picture-id91213977?b=1&k=20&m=91213977&s=170667a&w=0&h=_CqYRLIvq0MO_05-78PdBBJEYanCAwx5cX4ptsK0mbU=",
     subtitle: "2. Akubra Hats",
     text: "Made famous by the 1980s film franchise ‘Crocodile Dundee’ the Akubra hat is an Australian staple. Akubras are crafted from rabbit fur felt. Paired with a “driza-bone” oiled leather coat, Akubras are one half of the classic Australian stockman’s outfit, which was the official outfit of the 2007 APEC Leader’s conference held in Sydney. Akubra’s are wide brimmed, designed to ward off the burning Australian sun and typically have a thin leather band around the base of the crown. This is sometimes substituted for a bullet belt by the real outback “Aussies.” Akubra hats range in price from AU$110 to AU$164, depending on the style.",
     link: "https://akubra.com.au/",
+    id: 2,
   },
   {
     name: "Kangaroo Leather",
-    image: "",
+    image:
+      "https://www.bing.com/th?id=OIP.We1CELTgIBTBAhhb7USUKwHaF7&w=179&h=170&c=8&rs=1&qlt=90&o=6&dpr=1.25&pid=3.1&rm=2",
     subtitle: "3. Kangaroo Leather",
     text: "Sydney boasts some of the highest-quality kangaroo leather, carefully crafted into wallets, bags, belts and other goods. Kangaroo leather is prized because of its unique cross fibring, which allows it to be split very thinly without losing strength. It is considered the strongest type of leather for its weight. Prices vary according to the type of leather goods you choose, but a kangaroo leather wallet will set you back upwards of AU$29.95. A hand-embossed kangaroo leather wallet by a local aboriginal artist is AU$65.00.",
     link: "https://www.therealmccaul.com/pages/kangaroo-leather",
+    id: 3,
   },
   {
     name: "Gold Plated Eucalyptus Leaves",
-    image: "",
+    image:
+      "https://images.unsplash.com/photo-1520473378652-85d9c4aee6cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z29sZCUyMHBsYXRlZCUyMGV1Y2FseXRwdXMlMjBsZWF2ZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
     subtitle: "4. Gold Plated Eucalytus Leaves",
     text: "The eucalyptus tree is an Australian native, and Sydney is surrounded by ancient eucalypt forests. The skeletons of these eucalypt leaves are collected by Bush Art Australia and gold plated to form the most delicate and beautiful jewelry. Necklaces with a single leaf cost upward of AU$25.00, and a pair of earrings with one leaf on each earring - upward of $AU40.00. Brooches and necklace, and earring sets are also available, as well as gold-plated gumnuts.",
     link: "https://www.realleafjewelleryaustralia.com.au/products/copy-of-gold-eucalyptus-leaf-pendant",
+    id: 4,
   },
 ];
--- GSAP-project/js/main.js
+++ GSAP-project/js/main.js
@@ -1,39 +1,55 @@
-import "../js/array.js";
+import { attractions, foods, souvenirs } from "./array.js";
 
-const DOMSelectors = {
-  Attractions: document.querySelector(".Attractions"),
-  search: document.getElementById("search"),
-  foods: document.querySelector("foods"),
-  souvenirs: document.querySelector("souvenirs"),
-};
-console.log(DOMSelectors);
+for (let a in attractions) {
+  let heading = document.createElement("h2");
+  let img = document.createElement("img");
+  let text = document.createElement("div");
 
-function displayAttractions(Attractions) {
-  Attractions.forEach((Attractions, index) => {
-    document.querySelector("#container").insertAdjacentHTML(
-      "afterbegin",
-      `
-      id=${index}>
-      src="${Attractions.image}"/>
-      <h2>${Attractions.subtitle}</h2>
-    <h3>$${Attractions.text}</h3>
-    </div>`
-    );
-  });
+  heading.className = "attractions, heading";
+  img.className = "attractions, parent-1, img";
+  text.className = "attractions, parent-1, text";
+
+  heading.innerHTML = `${attractions[a].id}. ${attractions[a].name}`;
+  img.src = attractions[a].image;
+  text.innerHTML = attractions[a].text;
+
+  document.body.appendChild(heading);
+  document.body.appendChild(img);
+  document.body.appendChild(text);
 }
-displayAttractions(Attractions, "Attractions");
 
-function displayfoods(food) {
-  food.forEach((foods, index) => {
-    document.querySelector("#container").insertAdjacentHTML(
-      "afterbegin",
-      `
-      id=${index}>
-      src="${foods.image}"/>
-      <h2>${foods.subtitle}</h2>
-    <h3>$${foods.text}</h3>
-    </div>`
-    );
-  });
+for (let f in foods) {
+  let heading = document.createElement("h1");
+  let img = document.createElement("img");
+  let text = document.createElement("div");
+
+  heading.className = "foods, heading";
+  img.className = "foods, parent-2, img";
+  text.className = "parent-2, foods, text";
+
+  heading.innerHTML = `${foods[f].id}. ${foods[f].name}`;
+  img.src = foods[f].image;
+  text.innerHTML = foods[f].text;
+
+  document.body.appendChild(heading);
+  document.body.appendChild(img);
+  document.body.appendChild(text);
 }
-displayfoods(foods, "foods");
+
+for (let s in souvenirs) {
+  let heading = document.createElement("h1");
+  let img = document.createElement("img");
+  let text = document.createElement("div");
+
+  heading.className = "souvenirs, heading";
+  img.className = "souvenirs, parent-3, img";
+  text.className = "souvenirs, parent-3, text";
+
+  heading.innerHTML = `${souvenirs[s].id}. ${souvenirs[s].name}`;
+  img.src = souvenirs[s].image;
+  text.innerHTML = souvenirs[s].text;
+
+  document.body.appendChild(heading);
+  document.body.appendChild(img);
+  document.body.appendChild(text);
+}
